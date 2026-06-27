89f19ac7e11a1fee99556f085a50e41d883e46ea
ansarcodes <128506717+ansarcodes@users.noreply.github.com>
2026-06-27 14:09:27 +0500

    Source map for prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,5 +2,6 @@ import { merge } from "webpack-merge";
 import common from "./webpack.common.js";
 
 export default merge(common, {
-    mode: "production"
+    mode: "production",
+    devtool: "source-map"
 });
