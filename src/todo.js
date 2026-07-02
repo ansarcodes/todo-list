1f324a38a4ae5245dc29a75e6f6e370c5cf7d21a
ansarcodes <128506717+ansarcodes@users.noreply.github.com>
2026-07-02 11:40:17 +0500

    todo item class

---
+++ src/todo.js
@@ -0,0 +1,8 @@
+class todoItem {
+    constructor(title, description, dueDate, priority){
+        title = this.title;
+        description = this.description;
+        dueDate = this.dueDate;
+        priority = this.priority;
+    }
+}
