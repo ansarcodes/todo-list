620d4d83eb24153b47286d0d71fe81601ce2c2a8
ansarcodes <128506717+ansarcodes@users.noreply.github.com>
2026-07-11 13:06:07 +0500

    Project list, change application logic

--- src/application.js
+++ src/application.js
@@ -1,6 +1,53 @@
-export default function todoManipulation(todo){
-    const markComplete = () => {
+// export default function todoManipulation(todo){
+//     const markComplete = () => {
+//         todo.isComplete = !todo.isComplete;
+//     };
+//     return { markComplete };
+// };
+// const todoManipulation = (() => {
+//     const markComplete = (todo) => {
+//         todo.isComplete = !todo.isComplete;
+//     };
+//     return { markComplete };
+// })();
+// const todoManipulation = {
+//     markComplete(todo){
+//         todo.isComplete = !todo.isComplete;
+//     }
+// };
+// todoManipulation.markComplete= "123";
+
+const todoManipulation = (()=>{
+    const changeTitle = (todo, title) => {
+        todo.title = title;
+    }
+    const changeDescription = (todo, description) => {
+        todo.description = description;
+    }
+    const changeDueDate = (todo, dueDate) => {
+        todo.dueDate = dueDate;
+    }
+    const changePriority = (todo, priority) => {
+        todo.priority = priority;
+    }
+    const markComplete = (todo) => {
         todo.isComplete = !todo.isComplete;
     };
-    return { markComplete };
-};
+    return { changeTitle, changeDescription, changeDueDate, changePriority, markComplete };
+})();
+
+const projectList = (() => {
+    const projectListArray = [];
+    const get = () => {
+        return projectListArray;
+    }
+    const add = (project) => {
+        projectListArray.push(project);
+    }
+    const remove = (project) => {
+        projectListArray.splice(projectListArray.indexOf(project), 1);
+    }
+    return { get, add, remove };
+})();
+
+export { todoManipulation, projectList };
--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
 import TodoItem from "./todo.js";
 import Project from "./projects.js";
-import todoManipulation from "./application.js";
+import { todoManipulation, projectList } from "./application.js";
 
 let item = new TodoItem("test title", "test description", "01012027", "1");
 console.log(item.isComplete);
-todoManipulation(item).markComplete();
-console.log(item.isComplete);
+// todoManipulation.markComplete(item);
+// console.log(item.isComplete);
 // console.log(item.title);
 // console.log(item);
 
 
-// let defaultProject = new Project("Default");
-// defaultProject.addTodo(item);
-// console.log(defaultProject);
-// console.log(defaultProject.todoList);
+let defaultProject = new Project("Default");
+defaultProject.addTodo(item);
+console.log(defaultProject);
+console.log(defaultProject.todoList);
+
+projectList.add(defaultProject);
+console.log(projectList);
+console.log(projectList.get());
 
 // todoManipulation(item).markComplete;
 
+// console.log(item);
+
+// console.log(todoManipulation);
+// todoManipulation.markComplete = "ast";
+// console.log(todoManipulation);
+// todoManipulation.markComplete(item);
 // console.log(item);
