8951ecd8c5c6fe3d3637ece6b501beef94fc60b6
ansarcodes <128506717+ansarcodes@users.noreply.github.com>
2026-07-19 12:27:08 +0500

    Fix later

--- src/interface.js
+++ src/interface.js
@@ -2,4 +2,9 @@ function projectController(projectList, container){
     projectList.forEach(project => {
         container.appendChild(project);
     });
+};
+function todoController(todoList, container){
+    todoList.forEach(todo => {
+        container.appendChild(todo);
+    })
 }
