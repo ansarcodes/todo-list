9386d26e63454adc49a257dd529cfb6869f3f1e8
ansarcodes <128506717+ansarcodes@users.noreply.github.com>
2026-07-23 12:51:06 +0500

    Rework interface

--- src/interface.js
+++ src/interface.js
@@ -1,6 +1,24 @@
-function projectController(projectList, container){
-    projectList.forEach(project => {
-        container.appendChild(project);
+function projectController(projectList){
+    const projectListContainer = document.querySelector(".project-list");
+    projectList.forEach(projectItem => {
+        const project = document.createElement("div");
+        project.classList.add("project");
+        const projectName = document.createElement("div");
+        projectName.classList.add("project-name");
+        projectName.textContent = projectItem.name;
+        const projectTodoList = document.createElement("div");
+        projectTodoList.classList.add("project-todo-list");
+        projectItem.todoList.forEach(todoItem => {
+            const todo = document.createElement("div");
+            todo.classList.add("todo");
+            const todoName = document.createElement("div");
+            todoName.classList.add("todo-name");
+            const todoDueDate = document.createElement("div");
+            todoDueDate.classList.add("todo-due-date");
+            todo.appendChildren(todoName, todoDueDate);
+        })
+        project.appendChildren(projectName, projectTodoList);
+        projectListContainer.appendChild(project);
     });
 };
 function todoController(todoList, container){
