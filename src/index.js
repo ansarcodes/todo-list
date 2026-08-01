241592a821aec702eca3ec4735fe7ccd069065dd
ansarcodes <128506717+ansarcodes@users.noreply.github.com>
2026-08-01 12:11:55 +0500

    Change enter points, add render

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import "./style.css";
-import TodoItem from "./todo.js";
-import Project from "./projects.js";
-import { todoManipulation, projectList } from "./application.js";
-import { mainController, projectController } from "./interface.js";
+import renderPage from "./render.js";
+// import TodoItem from "./todo.js";
+// import Project from "./projects.js";
+// import { todoManipulation, projectList } from "./application.js";
+// import { mainController, projectController } from "./interface.js";
 
 // let item = new TodoItem("test title", "test description", "01012027", "1");
 // console.log(item.isComplete);
@@ -33,14 +34,15 @@ import { mainController, projectController } from "./interface.js";
 
 // projectController(projectList.get());
 
-let project1 = new Project("1");
-let item1 = new TodoItem("title", "description", "2026-07-25", "High");
-let item2 = new TodoItem("title2", "description2", "2026-07-25", "Mid");
-project1.addTodo(item1);
-project1.addTodo(item2);
-projectList.add(project1);
-console.log(project1.todoList);
-projectController(projectList.get());
-mainController(item1);
+// let project1 = new Project("1");
+// let item1 = new TodoItem("title", "description", "2026-07-25", "High");
+// let item2 = new TodoItem("title2", "description2", "2026-07-25", "Mid");
+// project1.addTodo(item1);
+// project1.addTodo(item2);
+// projectList.add(project1);
+// console.log(project1.todoList);
+// projectController(projectList.get());
+// mainController(item1);
 // console.log(projectList.get());
 // projectController(projectList.get());
+renderPage();
--- src/render.js
+++ src/render.js
@@ -1,3 +1,15 @@
+import TodoItem from "./todo.js";
+import Project from "./projects.js";
+import { todoManipulation, projectList } from "./application.js";
+import { mainController, projectController } from "./interface.js";
 export default function renderPage(){
-    
+    let project1 = new Project("1");
+    let item1 = new TodoItem("title", "description", "2026-07-25", "High");
+    let item2 = new TodoItem("title2", "description2", "2026-07-25", "Mid");
+    project1.addTodo(item1);
+    project1.addTodo(item2);
+    projectList.add(project1);
+    console.log(project1.todoList);
+    projectController(projectList.get());
+    mainController(item1);
 }
